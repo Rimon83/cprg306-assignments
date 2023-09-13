cba509c909f299ef684306a023575ce41fe28b1f
Rimon83 <111208690+Rimon83@users.noreply.github.com>
2023-09-12 18:54:30 -0600

    Complete Week-2 Assignment

---
+++ app/StudentInfo/page.js
@@ -0,0 +1,13 @@
+export default function StudentInfo(){
+ return (
+   <div>
+     <p>Name: Rimon Alqoshi</p>
+     <p>Course Section: CPRG 306 D</p>
+     <p>
+       <a href="https://github.com/Rimon83?tab=repositories">
+         https://github.com/Rimon83?tab=repositories
+       </a>
+     </p>
+   </div>
+ );
+} 
